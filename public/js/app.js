df36147631569dc8f080760b889863e3d72b7a8f
Nilesh Gawande <gawande74@gmail.com>
2020-07-02 15:51:04 +0530

    setup add for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     const location = search.value
 
     console.log('location')
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
 
     messageOne.textContent='loading...'
     fetch(url).then((response) => {
